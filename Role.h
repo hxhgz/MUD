db409724cf91bc87869f08c6160092f00d75a140
很喜欢格子 <150363653+hxhgz@users.noreply.github.com>
2024-08-26 20:56:45 +0800

    Update Role.h

--- Role.h
+++ Role.h
@@ -50,8 +50,9 @@ public:
 
 	void addSaveGoodsToBag(int Id, int num);//保存用
 
-	bag getBag();
+	Bag getBag();
 	void showBag();
+	void showDrug();
 	int getBagWhichGoodsId(int whichGoods);			//得到背包里第whichGoods件物品
 	int getBagWhichGoodsNum(int whichGoods);
 	//显示背包
@@ -60,10 +61,10 @@ public:
 
 	bool subGoodsToBag(int goodsId, int num);	//减少背包里的物品
 
-	void setBag(bag bags);
+	void setBag(Bag bags);
 
 	void savePlayerBag();		//用来单独保存背包
-	//int** getBagSave();
+
 	void newBag();
 
 	void showRole();//人物属性 当前装备
@@ -97,8 +98,8 @@ private:
 
 
 	int money;
-	bag bagbag;			//背包
-	//int bag[20];
+	Bag bagbag;			//背包
+	
 	int mapId;			//记录玩家当前所在地
 	Skill skill;		//他所拥有的技能
 
@@ -107,7 +108,7 @@ private:
 	int armor = -1; //当前护甲id
 
 	//物品实例化
-	goods goods[21] = { 0, 1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
+	Goods goods[27] = { 0, 1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20，21,22,23,24,25,26 };
 
 };
 #endif
