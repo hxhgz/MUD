42acd41d238ca5a512e97f4e8ca1acef992c51ae
zxzxzxz <3132568760@qq.com>
2024-08-26 20:49:51 +0800

    Update store.h

--- store.h
+++ store.h
@@ -1,3 +1,9 @@
+#ifndef STORE_H
+#define STORE_H
+
+
+
+
 #include<iostream>
 #include<map>
 #include"Goods.h"
@@ -14,8 +20,7 @@ public:
 private:
 	map<int, int> stores;
 
-
-
-	goods goods[21] = { 0, 1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+	Goods goods[27] = { 0, 1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26 };
 };
-#
+
+#endif // !STORE_H
